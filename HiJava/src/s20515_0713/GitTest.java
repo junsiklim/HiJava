1e48a3edda5379a8819851bd78781581283829e7
junsik <junsik@218.49.122.27>
2021-07-13 10:11:57 +0900

    첫번째 깃 소스임.

---
+++ HiJava/src/s20515_0713/GitTest.java
@@ -0,0 +1,10 @@
+package s20515_0713;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("¾È³ç ±êÇãºê Ã¹¹øÂ°");
+	}
+
+}
