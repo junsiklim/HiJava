f08429796572310b90a56aec23dc706b7708d11b
junsik <junsik@218.49.122.27>
2021-07-13 10:34:45 +0900

    두번째 소스

--- HiJava/src/s20515_0713/GitTest.java
+++ HiJava/src/s20515_0713/GitTest.java
@@ -5,6 +5,8 @@ public class GitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("안녕 깃허브 첫번째");
+		
+		System.out.println("두번째 깃 허브");
 	}
 
 }
